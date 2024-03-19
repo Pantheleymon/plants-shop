3a51f1b85a6b58e9f38d940f5a516016792d0f2f
Pantheleymon <pantheleymonbbx@yandex.ru>
2024-03-20 00:41:11 +0300

    refactor: Улучшение читаемости кода

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import './header.module.scss';
+import styles from './Header.module.scss';
 
 import { Link, useLocation } from "react-router-dom";
 
@@ -8,21 +8,37 @@ const Header = () => {
     const {pathname} = useLocation();
 
     return (
-        <header className="flex justify-between content-center py-10 text-2xl font-bold">
-            <div className="flex justify-start content-center">
-                <div className="logo">
-                    <Link to="/">GREENMIND</Link>
-                </div>
+        <header className={styles.header}>
+            <div className={styles.menu}>
+                <Link to="/">GREENMIND</Link>
 
-                <nav className='flex justify-between gap-12 ml-24 font-normal text-gray-400 pb-3'>
-                    <Link to="/" className='pb-2 hover:border-b-2 hover:border-black transition-all' style={{color: pathname === '/' ? 'black' : ''}}>Home</Link>
-                    <Link to="/products" className='pb-2 hover:border-b-2 hover:border-black transition-all' style={{color: pathname === '/products' ? 'black' : ''}}>Products</Link>
-                    <Link to="/contacts" className='pb-2 hover:border-b-2 hover:border-black transition-all' style={{color: pathname === '/contacts' ? 'black' : ''}}>Contacts</Link>
+                <nav className={styles.nav}>
+                    <Link 
+                        to="/" 
+                        className={styles.link} 
+                        style={{color: pathname === '/' ? 'black' : ''}}>
+                            Home
+                    </Link>
+                    <Link 
+                        to="/products" 
+                        className={styles.link} 
+                        style={{color: pathname === '/products' ? 'black' : ''}}>
+                            Products
+                    </Link>
+                    <Link 
+                        to="/contacts" 
+                        className={styles.link} 
+                        style={{color: pathname === '/contacts' ? 'black' : ''}}>
+                            Contacts
+                    </Link>
                 </nav>
             </div>
-            <div className="cursor-pointer text-black hover:bg-gray-100 hover:shadow-md rounded-full p-2 h-fit">
-                <Link to='/cart'>
-                    <BsCart height={24} width={24}/>
+            <div className={styles.cart}>
+                <Link 
+                    to='/cart'>
+                    <BsCart 
+                        height={24} 
+                        width={24}/>
                 </Link>
             </div>
         </header>
